11639dfef303cc08f136ce72eb8e37605dd07256
unknown <IGDA@AS409-020.ds.swin.edu.au>
2015-01-24 17:42:08 +1100

    Levels enumerates.

---
+++ Assets/Scripts/GameController/Levels.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Levels : MonoBehaviour
+{
+	public const string Level1 = "Basics";
+	public const string Level2 = "JumpTutorial";
+	public const string Level3 = "LeftRightTutorial";
+	public const string Level4 = "ShootTutorial";
+	public const string Level5 = "SightTutorial";
+	//public const string Level6 = "";
+	//public const string Level7 = "";
+	//public const string Level8 = "";
+}
