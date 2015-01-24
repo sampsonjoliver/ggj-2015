dbcd14b400caf20b5a98efea455a8b1f6e7cb4d8
Sam <sam.oliver1993@gmail.com>
2015-01-24 11:38:40 +1100

    Added      Assets/Scripts/GameController/ModifierActions.cs Added      Assets/Scripts/Modifier/ModifierBehaviour.cs Added      Assets/Scripts/Player/PlayerInput.cs Modified   Assets/Prefab/Ground.prefab Modified   Assets/Scripts/Tags.cs

---
+++ Assets/Scripts/Modifier/ModifierBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModifierBehaviour : MonoBehaviour {
+    public string actionModified;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
