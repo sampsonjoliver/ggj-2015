95afca0bb6e7a220a0195829f4930853412c9421
Sam <sam.oliver1993@gmail.com>
2015-01-24 15:28:39 +1100

    Modified   Assets/Scripts/Modifier/ModifierBehaviour.cs

--- Assets/Scripts/Modifier/ModifierBehaviour.cs
+++ Assets/Scripts/Modifier/ModifierBehaviour.cs
@@ -3,14 +3,32 @@ using System.Collections;
 
 public class ModifierBehaviour : MonoBehaviour {
     public string actionModified;
+    private ModifierActions modifierActions;
+    private Collider2D collider;
 
 	// Use this for initialization
 	void Start () {
-	    
+        collider = GetComponent<Collider2D>();
+        modifierActions = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<ModifierActions>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other == modifiedObject)
+        {
+            modifierActions.setActionEnabled(actionModified, false);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other == modifiedObject)
+        {
+            modifierActions.setActionEnabled(actionModified, true);
+        }
+    }
 }
