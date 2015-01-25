38b5c0e3807787df248603736ed392b5f0894250
Sam <sam.oliver1993@gmail.com>
2015-01-25 15:23:17 +1100

    Add        Assets/Scenes/Main.unity Add        Assets/Scenes/Main.unity.meta Add        Assets/Scripts/GameController/TitleHandler.cs Add        Assets/Scripts/GameController/TitleHandler.cs.meta

---
+++ Assets/Scripts/GameController/TitleHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleHandler : MonoBehaviour {
+    public float fadeInTime = 4f;
+    bool displayingStartText;
+    public Text startText;
+    private string levelName = "basics";
+
+    public float timer = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (!displayingStartText && timer >= fadeInTime)
+        {
+            startText.enabled = true;
+            displayingStartText = true;
+        }
+
+        if (displayingStartText)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Application.LoadLevel(levelName);
+            }
+        }
+	}
+}
